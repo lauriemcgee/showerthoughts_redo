bf37fed678e725ec10ad85622370f6f4a9b51a62
lauriemcgee <livingoncoffeeandflowers@gmail.com>
2017-03-21 17:44:16 -0500

    add edit, new, and compelte index pages

--- app/controllers/shower_thoughts_controller.rb
+++ app/controllers/shower_thoughts_controller.rb
@@ -1,20 +1,31 @@
 class ShowerThoughtsController < ApplicationController
   def index
+    @showerthoughts = ShowerThought.all
     render "index.html.erb"
   end
   def new
-    redirect_to "index.html.erb"
+    render "new.html.erb"
   end
   def create
-    render "create.html.erb"
+    @showerthought = ShowerThought.create(
+      thought: params[:thought]
+      )
+    redirect_to "index.html.erb"
   end
   def edit
+    @showerthought = ShowerThought.find_by(:id params[:id])
     render "edit.html.erb"
   end
   def update
+    @showerthought = ShowerThought.find_by(:id params[:id])
+    @showerthought.update(
+      thought: params[:thought]
+      )
     redirect_to "index.html.erb"
   end
   def destroy
+    @showerthought = ShowerThought.find_by(:id params[:id])
+    @showerthought.destroy
     redirect_to "index.html.erb"
   end
 
